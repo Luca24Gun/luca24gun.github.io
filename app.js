535dc2f1fc1a9cfc1084f8ff628fa1c79473836a
Luca Mirra <luca.mirra@icloud.com>
2023-08-24 09:07:19 +0200

    nouveau design pour les projets plus evolution du js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ console.log(side.style);
 me.addEventListener("mouseover", function(){
     console.log("ok");
     side.style.marginLeft = "0px";
-})
+});
 side.addEventListener("mouseover", function(){
     side.style.marginLeft = "0px";
 });
@@ -21,9 +21,55 @@ setInterval(displayHour, 1000);
 
 function displayHour() {
     var now = new Date();
-    var heure = now.getHours();
+    var heures = now.getHours();
     var minutes = now.getMinutes();
     var secondes = now.getSeconds();
+    if(heures < 10) {heures = '0' + heures;}
+    if(minutes < 10) {minutes = '0' + minutes;}
+    if(secondes < 10) {secondes = '0' + secondes;}
     var hour = document.getElementById("hour");
-    hour.innerHTML = heure + ":" + minutes + ":" + secondes;
-}
+    hour.innerHTML = heures + ":" + minutes + ":" + secondes;
+};
+
+function sentence() {
+
+};
+
+var prj1 = document.getElementById("prj1");
+var txt1 = document.getElementById('txt1');
+var imgprj1 = document.getElementById("img-prj1")
+console.log(side.style);
+prj1.addEventListener("click", function(){
+    if (txt1.style.display === "none")
+        txt1.style.display = "block";
+    else
+        txt1.style.display = "none";
+});
+prj1.addEventListener("mouseover", function(){
+    imgprj1.style.filter = "blur(0)";
+});
+prj1.addEventListener("mouseout", function(){
+    imgprj1.style.filter = "blur(5px)";
+});
+
+var prj2 = document.getElementById("prj2");
+var txt2 = document.getElementById('txt2');
+console.log(side.style);
+prj2.addEventListener("click", function(){
+    console.log("ok");
+    if (txt2.style.display === "none")
+        txt2.style.display = "block";
+    else
+        txt2.style.display = "none";
+});
+
+var prj3 = document.getElementById("prj3");
+var txt3 = document.getElementById('txt3');
+console.log(side.style);
+prj3.addEventListener("click", function(){
+    console.log("ok");
+    if (txt3.style.display === "none")
+        txt3.style.display = "block";
+    else
+        txt3.style.display = "none";
+});
